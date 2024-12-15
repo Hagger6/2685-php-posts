7652215515df2de46908da56fe664e88d30d5bef
Hagar <hagar.essam.se@gmail.com>
2024-12-15 18:44:34 +0200

    more updates

--- components/post.php
+++ components/post.php
@@ -1,7 +1,7 @@
 <div style="border: solid 1px; margin: 20px 0; max-width: 300px;">
-    <h2><?= $user['title']; ?></h2>
-    <p><?= $user['body']; ?></p>
-    <p><?= $user['user_id']; ?></p>
-    <p><?= $user['post_status_id']; ?></p>
-    <a href="/user.php?id=<?=$user['user_id']?>">View</a>
+    <h2><?= $post['title']; ?></h2>
+    <p><?= $post['body']; ?></p>
+    <p><?= $post['user_id']; ?></p>
+    <p><?= $post['post_status_id']; ?></p>
+    <a href="/post.php?id=<?=$post['id']?>">View</a>
 </div>
