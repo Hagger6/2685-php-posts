664e9461a215e5d89224d6dfc28c20e6cba1d6d8
Hagar <hagar.essam.se@gmail.com>
2024-12-17 22:07:55 +0200

    title styles

--- public/index.php
+++ public/index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to my home page</title>
+    <title>Home page</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    
+    <p class="text-4xl md:text-5xl font-extrabold text-gray-800 text-center leading-tight">Welcome to my home page</p>
 </body>
 </html>
