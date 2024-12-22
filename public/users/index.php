fd6198f0beb454bdc5cdfbd4c7509f787edaa0c0
Hagar <hagar.essam.se@gmail.com>
2024-12-22 17:29:58 +0200

    session 12 update

--- public/users/index.php
+++ public/users/index.php
@@ -1,13 +1,9 @@
 <?php
-include '../load.php';
+include '../../load.php';
 
 $qry = 'SELECT * FROM `pst_users` LIMIT 10;';
 
-$res = $db->query($qry);
-
-$users = mysqli_fetch_all($res, MYSQLI_ASSOC);
-
 
 foreach ($users as $user) {
-    include '../components/users/user.php';
+    include '../../components/users/user.php';
 }
